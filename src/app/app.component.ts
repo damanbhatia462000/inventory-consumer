7b309cc17cd6593a32475b2ee641680af7f7655b
Daman <daman.bhatia46@gmail.com>
2022-05-10 13:13:01 +0530

    Inventory consumer 1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from './authentication.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'inventory-consumer';
+  title = 'Product Inventory Management System';
+  imagePath='/assets/images/ims.jpg';
+
+  constructor(public loginService:AuthenticationService) { }
 }
